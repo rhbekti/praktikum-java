7447de85013c2a025ddb107e4926b24a4fe46064
rahman pambekti <67356011+rhbekti@users.noreply.github.com>
2021-11-18 08:47:03 +0700

    add praktikum 9

---
+++ praktikum-9/UlangDo1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UlangDo1{
+    public static void main(String[] args){
+        Scanner masuk = new Scanner(System.in);
+        int bil;
+        bil = 1;
+        do{
+            System.out.println(bil);
+            bil+=2;
+        } while(bil <= 10);
+    }
+}
